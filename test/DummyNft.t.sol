35a8666859a7995c8a458bfb3fb522d34dd37e30
zelinarxy <zelinarxy@proton.me>
2026-01-14 19:54:38 -0500

    cleam up

--- test/DummyNft.t.sol
+++ test/DummyNft.t.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.28;
 import {Test} from "forge-std/Test.sol";
 import "../src/dummy/DummyNft.sol";
 
-// TODO: mismatch messages on assertEq
 contract DummyNftTest is Test {
     DummyNft private _dummyNft;
     address alice = makeAddr("Alice");
@@ -14,20 +13,20 @@ contract DummyNftTest is Test {
     }
 
     function testName() public {
-        assertEq(_dummyNft.name(), "DummyNft");
+        assertEq(_dummyNft.name(), "DummyNft", "Name mismatch");
     }
 
     function testSymbol() public {
-        assertEq(_dummyNft.symbol(), "DUMMY");
+        assertEq(_dummyNft.symbol(), "DUMMY", "Symbol mismatch");
     }
 
     function testMintTo() public {
         _dummyNft.mintTo(address(alice));
-        assertEq(_dummyNft.balanceOf(address(alice)), 1);
+        assertEq(_dummyNft.balanceOf(address(alice)), 1, "Balance mismatch");
     }
 
     function testTokenURI() public {
         _dummyNft.mintTo(address(alice));
-        assertEq(_dummyNft.tokenURI(1), "");
+        assertEq(_dummyNft.tokenURI(1), "", "URI mismatch");
     }
 }
