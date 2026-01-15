08f4e3ae10495dc4f0f46e0a38b2e522fc0f4062
zelinarxy <zelinarxy@proton.me>
2026-01-14 19:52:43 -0500

    test cleanup stuff

--- test/Humbaba.t.sol
+++ test/Humbaba.t.sol
@@ -7,57 +7,57 @@ import "../src/Humbaba.sol";
 
 // TODO: mismatch messages on assertEq
 contract HumbabaTest is Ownable, Test {
-    Humbaba public humbaba;
+    Humbaba private _humbaba;
     address alice = makeAddr("Alice");
     address bob = makeAddr("Bob");
     address mallory = makeAddr("Mallory");
 
     function setUp() public {
         vm.prank(alice);
-        humbaba = new Humbaba("https://foo.bar/");
+        _humbaba = new Humbaba("https://foo.bar/");
     }
 
     function testName() public {
-        assertEq(humbaba.name(), "Humbaba", "Name mismatch");
+        assertEq(_humbaba.name(), "Humbaba", "Name mismatch");
     }
 
     function testSymbol() public {
-        assertEq(humbaba.symbol(), "HUMB", "Symbol mismatch");
+        assertEq(_humbaba.symbol(), "HUMB", "Symbol mismatch");
     }
 
     function testAdminMintTo() public {
         vm.prank(alice);
-        humbaba.adminMintTo(address(bob));
-        assertEq(humbaba.balanceOf(address(bob)), 1, "Bob balance mismatch");
+        _humbaba.adminMintTo(address(bob));
+        assertEq(_humbaba.balanceOf(address(bob)), 1, "Bob balance mismatch");
     }
 
     function testAdminMintToNotOwner() public {
         vm.prank(mallory);
         vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector));
-        humbaba.adminMintTo(address(bob));
+        _humbaba.adminMintTo(address(bob));
     }
 
     function testTokenURI() public {
         vm.prank(alice);
-        humbaba.adminMintTo(address(bob));
-        assertEq(humbaba.tokenURI(1), "https://foo.bar/1", "URI mismatch");
+        _humbaba.adminMintTo(address(bob));
+        assertEq(_humbaba.tokenURI(1), "https://foo.bar/1", "URI mismatch");
     }
 
     function testTokenURINonExistent() public {
         vm.expectRevert(abi.encodeWithSelector(NonExistentToken.selector));
-        humbaba.tokenURI(2);
+        _humbaba.tokenURI(2);
     }
 
     function testUpdateBaseURI() public {
-        assertEq(humbaba.baseURI(), "https://foo.bar/", "Before URI mismatch");
+        assertEq(_humbaba.baseURI(), "https://foo.bar/", "Before URI mismatch");
         vm.prank(alice);
-        humbaba.updateBaseURI("https://baz.qux/");
-        assertEq(humbaba.baseURI(), "https://baz.qux/", "After URI mismatch");
+        _humbaba.updateBaseURI("https://baz.qux/");
+        assertEq(_humbaba.baseURI(), "https://baz.qux/", "After URI mismatch");
     }
 
     function testUpdateBaseURINotOwner() public {
         vm.prank(mallory);
         vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector));
-        humbaba.updateBaseURI("https://baz.qux/");
+        _humbaba.updateBaseURI("https://baz.qux/");
     }
 }
