e12a2ce64825fafed0a081fa59cd4dc2436f459c
zelinarxy <zelinarxy@proton.me>
2026-02-02 23:33:33 -0500

    update humbaba test names

--- test/Humbaba.t.sol
+++ test/Humbaba.t.sol
@@ -17,45 +17,45 @@ contract HumbabaTest is Ownable, Test {
         _humbaba = new Humbaba("https://foo.bar/");
     }
 
-    function testName() public {
+    function test_name_returnsName() public {
         assertEq(_humbaba.name(), "Humbaba", "Name mismatch");
     }
 
-    function testSymbol() public {
+    function test_symbol_returnsSymbol() public {
         assertEq(_humbaba.symbol(), "HUMB", "Symbol mismatch");
     }
 
-    function testAdminMintTo() public {
+    function test_adminMintTo_succeeds() public {
         vm.prank(alice);
         _humbaba.adminMintTo(address(bob));
         assertEq(_humbaba.balanceOf(address(bob)), 1, "Bob balance mismatch");
     }
 
-    function testAdminMintToNotOwner() public {
+    function test_adminMintTo_reverts_whenCallerIsNotOwner() public {
         vm.prank(mallory);
         vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector));
         _humbaba.adminMintTo(address(bob));
     }
 
-    function testTokenURI() public {
+    function test_tokenURI_returnsTokenURI() public {
         vm.prank(alice);
         _humbaba.adminMintTo(address(bob));
         assertEq(_humbaba.tokenURI(1), "https://foo.bar/1", "URI mismatch");
     }
 
-    function testTokenURINonExistent() public {
+    function test_tokenURI_reverts_whenTokenDoesNotExist() public {
         vm.expectRevert(abi.encodeWithSelector(NonExistentToken.selector));
         _humbaba.tokenURI(2);
     }
 
-    function testUpdateBaseURI() public {
+    function test_updateBaseURI_updatesCorrectly() public {
         assertEq(_humbaba.baseURI(), "https://foo.bar/", "Before URI mismatch");
         vm.prank(alice);
         _humbaba.updateBaseURI("https://baz.qux/");
         assertEq(_humbaba.baseURI(), "https://baz.qux/", "After URI mismatch");
     }
 
-    function testUpdateBaseURINotOwner() public {
+    function test_updateBaseURI_reverts_whenCallerIsNotOwner() public {
         vm.prank(mallory);
         vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector));
         _humbaba.updateBaseURI("https://baz.qux/");
